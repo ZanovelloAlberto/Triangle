b544dd0933d1a050887f3831f553d3620738a7ae
Alberto Zanovello <zanovello2002@gmail.com>
2021-11-10 23:07:44 +0100

    done boy

--- src/program.cs
+++ src/program.cs
@@ -2,29 +2,54 @@ using System;
 
 namespace Triangle
 {
-    class Program{
-        static void Main(string[] args){
-            
-            
-            while(true){
-                
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
                 Console.WriteLine("Insert the lenght of the sides");
-            
+
                 int i = 3;
                 var a = new int[3];
 
-                while(i-->0){
-                    Console.WriteLine($"side number: {3-i}");
-                    a[i] = Convert.ToInt32(Console.ReadLine());
+                while (i-- > 0)
+                {
+                    Console.WriteLine($"side {3 - i}");
+                    a[i] = ReadLine();
                 }
                 var tri = new Triangle(a);
 
-
-                Console.WriteLine($"Area:       {tri.GetArea()}");
-                Console.WriteLine($"Max Side:   {tri.GetMaxSide()}");
-                Console.WriteLine($"Perimeters: {tri.GetPerimeters()}");
-
+                if (tri.IsTriangle())
+                {
+                    Console.WriteLine($"Area:           {tri.GetArea()}");
+                    Console.WriteLine($"Max Side:       {tri.GetMaxSide()}");
+                    Console.WriteLine($"Perimeters:     {tri.GetPerimeters()}");
+                    Console.WriteLine($"");
+                    Console.WriteLine($"Isosceles:      {tri.IsIsosceles()}");
+                    Console.WriteLine($"Equilateral:    {tri.IsEquilateral()}");
+                }
+                else
+                {
+                    Console.WriteLine($"sides lenght dont mach with a triangle");
+                }
+                Console.WriteLine($"============= DONE ============== \n");
             }
+        }
+        private static int ReadLine()
+        {
+            while (true)
+            {
+                try
+                {
+                    return int.Parse(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine($"insert an Intger");
+                }
+            }
+
         }
     }
 }
--- src/triangle.cs
+++ src/triangle.cs
@@ -4,39 +4,85 @@ using System;
 namespace Triangle
 {
 
-    class Triangle{
+    class Triangle
+    {
         public int[] sides;
 
-        public Triangle(int[] sides){
+        public Triangle(int[] sides)
+        {
             this.sides = sides;
-            
         }
-        public int GetPerimeters(){
+        public int GetPerimeters()
+        {
             int a = 0;
             for (int i = 0; i < sides.Length; i++)
             {
-                a+=sides[i];
+                a += sides[i];
             }
-            return a;   
+            return a;
         }
-        public double GetArea(){
+        public double GetArea()
+        {
             // semiperimeters
-            var p = GetPerimeters()/2;
-            
+            var p = GetPerimeters() / 2;
+
             // @@@ Erone formula @@@
-            return Math.Pow(p*(p-sides[0])*(p-sides[1])*(p-sides[2]),0.5);
+            return Math.Pow(p * (p - sides[0]) * (p - sides[1]) * (p - sides[2]), 0.5);
         }
-        
-        public int GetMaxSide(){
+
+        public int GetMaxSide()
+        {
             // not efficent just readable
             int i = 3;
             int res = 0;
-            while(--i>0){
+            while (i-- > 0)
+            {
                 res = res > sides[i] ? res : sides[i];
             }
             return res;
         }
+
+        public bool IsTriangle()
+        {
+            int i = 3;
+            while (i-- > 0)
+            {
+                if (sides[i] > sides[(i + 1) % 3] + sides[(i + 1) % 3])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        public bool IsIsosceles()
+        {
+            int i = 3;
+            if(IsEquilateral()){
+                return false;
+            }
+            while (i-- > 0)
+            {
+                if (sides[i] == sides[(i + 1) % 3])
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public bool IsEquilateral()
+        {
+
+                if (sides[0] == sides[1] && sides[1] == sides[2])
+                {
+                    return true;
+                }
+            
+            return false;
+        }
+        
+        
+
     }
 
-    
+
 }
